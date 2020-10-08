4b2b6da2ad666b3da20331c24beef3dc41e6307e
matata samuel <mattasamuel3@gmail.com>
2020-10-08 21:19:35 +0300

    added article

--- news/models.py
+++ news/models.py
@@ -50,3 +50,5 @@ class Article(models.Model):
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
+
+        
--- news/urls.py
+++ news/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
- 
+urlpatterns=[      
     url(r'^$',views.news_of_day,name='newsToday'),    
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^article/(\d+)',views.article,name ='article')
 ]
 
 if settings.DEBUG:
