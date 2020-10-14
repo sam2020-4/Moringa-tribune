d1bca8115a0045f261f00711e3e8ac1cded75961
matata samuel <mattasamuel3@gmail.com>
2020-10-14 21:18:06 +0300

    added form.py

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from .forms import NewsLetterForm
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
@@ -11,28 +12,20 @@ def news_of_day(request):
     date = dt.date.today()
     return render(request, 'all-news/today-news.html', {"date": date,})
 
-# def past_days_news(request, past_date):
-
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-#     except ValueError:
-#         # Raise 404 error when ValueError is thrown
-#         raise Http404()
-#         assert False
-
-#     if date == dt.date.today():
-#         return redirect(news_of_day)
-
-#     return render(request, 'all-news/past-news.html', {"date": date})
 
 # updating views
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+    # return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 def past_days_news(request, past_date):
     try:
