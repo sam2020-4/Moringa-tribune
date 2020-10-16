6bfa96f79862360aac241bb5aabd58d9ec56c0b3
matata samuel <mattasamuel3@gmail.com>
2020-10-16 07:52:54 +0300

    validated email

--- news/views.py
+++ news/views.py
@@ -25,8 +25,11 @@ def news_today(request):
         if form.is_valid():
             name = form.cleaned_data['your_name']
             email = form.cleaned_data['email']
-            recipient = NewsLetterRecipients( name = name,email =email)
+            
+            recipient = NewsLetterRecipients(name = name,email =email)
             recipient.save()
+            send_welcome_email(name,email)
+            
             HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
